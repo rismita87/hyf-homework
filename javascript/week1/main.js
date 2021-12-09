b6580ee047501dad42060e2eef9c5df9eec01f45
rismita87 <rismita.ghosh@gmail.com>
2021-12-09 12:45:14 +0100

    created the index.html and main.js. in main.js --> added code for Age-ify (A future age calculator)

---
+++ javascript/week1/main.js
@@ -0,0 +1,5 @@
+// Age-ify (A future age calculator)
+let yearOfBirth = 1988; // it can change so "let" variable
+const yearFuture = 2027; // as the future year given so "const" variable
+let age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old on " + yearFuture + ".");
