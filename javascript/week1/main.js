fc63a4aa7436e315b4aac0dc9b5e54eab57a4c0a
rismita87 <rismita.ghosh@gmail.com>
2021-12-09 12:54:08 +0100

    added code for Goodboy-Oldboy (A dog age calculator)

--- javascript/week1/main.js
+++ javascript/week1/main.js
@@ -3,3 +3,20 @@ let yearOfBirth = 1988; // it can change so "let" variable
 const yearFuture = 2027; // as the future year given so "const" variable
 let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old on " + yearFuture + ".");
+
+//Goodboy-Oldboy (A dog age calculator)
+let dogYearOfBirth = 2017; // in human years and as it can change so "let" variable
+const dogYearFuture = 2027; // as the future year given so "const" variable
+let dogYear;
+let shouldShowResultInDogYears = false; //boolean type variable.
+if (shouldShowResultInDogYears) {
+  dogYear = (dogYearFuture - dogYearOfBirth) * 7;
+  console.log(
+    "Your dog will be " + dogYear + " dog years old in " + dogYearFuture
+  );
+} else {
+  dogYear = dogYearFuture - dogYearOfBirth;
+  console.log(
+    "Your dog will be " + dogYear + " human years old in " + dogYearFuture
+  );
+}
