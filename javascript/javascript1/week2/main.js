f18e31f91f1758bcd60a546acd78f349ea47516c
rismita87 <rismita.ghosh@gmail.com>
2021-12-17 20:46:54 +0100

    event application coding file added

--- javascript/javascript1/week2/main.js
+++ javascript/javascript1/week2/main.js
@@ -26,3 +26,60 @@ function getFullname(
 
 const fullname1 = getFullname("Aaditri", "Sarkar", true, true);
 const fullname2 = getFullname(" ");
+
+//Event application
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let todayNumber;
+function today(day) {
+  todayNumber = days.indexOf(day) + 1;
+}
+
+let eventday;
+function eventWeekday(id) {
+  switch (id) {
+    case 1:
+      eventday = "Sunday";
+      break;
+    case 2:
+      eventday = "Monday";
+      break;
+    case 3:
+      eventday = "Tuesday";
+      break;
+    case 4:
+      eventday = "Wednesday";
+      break;
+    case 5:
+      eventday = "Thursday";
+      break;
+    case 6:
+      eventday = "Friday";
+      break;
+    case 7:
+      eventday = "Saturday";
+  }
+  return eventday;
+}
+
+let eventDayWouldBe;
+function getEventWeekday(eventdayNumber) {
+  let dayNumber = eventdayNumber + todayNumber;
+  if (dayNumber > 6) {
+    eventDayWouldBe = dayNumber % 7;
+    console.log("Event day will be " + eventWeekday(eventDayWouldBe));
+  } else {
+    eventDayWouldBe = dayNumber;
+    console.log("Event day will be " + eventWeekday(eventDayWouldBe));
+  }
+}
+
+today("Sunday");
+getEventWeekday(8);
