952fb443bd46fdb4922171bd86c2f05e6a3a955c
rismita87 <rismita.ghosh@gmail.com>
2022-01-14 22:13:50 +0100

    code added for When will we be there??

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -17,3 +17,19 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 console.log(names);
+
+//When will we be there??
+const travelInformation = {
+  speed: 60,
+  destinationDistance: 567,
+};
+
+function travelTimeCalculation() {
+  var hours = (distance - (distance % speed)) / speed;
+  var minutes = ((distance % speed) * 60) / speed;
+  return [hours, minutes];
+}
+let distance = travelInformation.destinationDistance;
+let speed = travelInformation.speed;
+const travelTime = travelTimeCalculation(travelInformation);
+console.log(travelTime[0] + " hours and " + travelTime[1] + " minutes.");
