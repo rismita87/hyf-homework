33483253c5fa4499e8a8b7481a60ab1c23f5c8f5
rismita87 <rismita.ghosh@gmail.com>
2022-01-14 22:11:02 +0100

    code added for Item array removal

---
+++ javascript/javascript1/week3/main.js
@@ -0,0 +1,19 @@
+//Item array removal
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = "Ahmad";
+for (var i = 0; i < names.length; i++) {
+  if (names[i] === nameToRemove) {
+    names.splice(i, 1);
+    i--;
+  }
+}
+console.log(names);
