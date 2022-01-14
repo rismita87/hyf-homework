e12dc233aa0ba45bce4edf23722bcb5de1d118a5
rismita87 <rismita.ghosh@gmail.com>
2022-01-14 22:16:11 +0100

    code added for Series duration of my life

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -33,3 +33,52 @@ let distance = travelInformation.destinationDistance;
 let speed = travelInformation.speed;
 const travelTime = travelTimeCalculation(travelInformation);
 console.log(travelTime[0] + " hours and " + travelTime[1] + " minutes.");
+
+//Series duration of my life
+const seriesDurations = [
+  {
+    title: "The Mentalist",
+    days: 4,
+    hours: 12,
+    minutes: 13,
+  },
+  {
+    title: "Kota Factory",
+    days: 0,
+    hours: 7,
+    minutes: 30,
+  },
+  {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "Money Heist",
+    days: 1,
+    hours: 23,
+    minutes: 50,
+  },
+];
+
+function logOutSeriesText() {
+  for (var i = 0; i < seriesDurations.length; i++) {
+    var seriesDurationInMinutes =
+      seriesDurations[i].days * 24 * 60 +
+      seriesDurations[i].hours * 60 +
+      seriesDurations[i].minutes;
+    var percentageTaken =
+      (seriesDurationInMinutes / avarageLifeSpanInMinutes) * 100;
+    totalPercentage += percentageTaken;
+    return (
+      seriesDurations[i].title + " took " + percentageTaken + "% of my life."
+    );
+  }
+  return "In total that is " + totalPercentage + "% of my life.";
+}
+var totalPercentage = 0;
+const avarageLifeSpan = 80;
+const avarageLifeSpanInMinutes = 80 * 365 * 24 * 60;
+
+console.log(logOutSeriesText());
