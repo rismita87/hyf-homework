f413b8ae30659e928f2cee3069aa4722be5b7d44
rismita87 <rismita.ghosh@gmail.com>
2022-03-04 15:20:25 +0100

    js file for Getting into promises HW added

---
+++ javascript/javascript3/week3/promise_main.js
@@ -0,0 +1,33 @@
+//Getting into promises
+
+let promiseAll = Promise.all([
+  fetch(
+    " https://api.github.com/search/repositories?q=user:SvitlanaBurlaka"
+  ).then((response) => response.json()),
+  fetch(" https://api.github.com/search/repositories?q=user:alina-kho").then(
+    (response) => response.json()
+  ),
+  fetch(
+    " https://api.github.com/search/repositories?q=user:TatjanaOcedova"
+  ).then((response) => response.json()),
+]);
+
+const body = document.querySelector("body");
+const ulToRenderInfo = document.createElement("ul");
+body.appendChild(ulToRenderInfo);
+
+promiseAll.then((data) => {
+  data.forEach((element) => {
+    console.log(element.items);
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${element.items[0].owner.login}'s repository`;
+    const ulToRenderInfoForeachRepo = document.createElement("ul");
+    listItem.appendChild(ulToRenderInfoForeachRepo);
+    element.items.forEach((item) => {
+      const listItemForEachRepo = document.createElement("li");
+      listItemForEachRepo.innerHTML = `${item.name}: ${item.html_url}`;
+      ulToRenderInfoForeachRepo.appendChild(listItemForEachRepo);
+    });
+    ulToRenderInfo.appendChild(listItem);
+  });
+});
