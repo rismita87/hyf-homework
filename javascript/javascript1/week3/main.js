3197b4338f31dc75aa697e8a67716c2eb2fb1565
rismita87 <rismita.ghosh@gmail.com>
2022-03-22 15:16:02 +0100

    changes made to resolve conflicts

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -62,39 +62,40 @@ const seriesDurations = [
     minutes: 50,
   },
 ];
-
-function logOutSeriesText() {
-  for (var i = 0; i < seriesDurations.length; i++) {
-    var seriesDurationInMinutes =
+function logOutSeriesText(avarageLifeSpan) {
+  const arrayWithTotalAndIndividualShow = [];
+  const arrayWithIndividualShow = [];
+  let totalPercentage = 0;
+  for (let i = 0; i < seriesDurations.length; i++) {
+    const avarageLifeSpanInMinutes = avarageLifeSpan * 365 * 24 * 60;
+    const seriesDurationInMinutes =
       seriesDurations[i].days * 24 * 60 +
       seriesDurations[i].hours * 60 +
       seriesDurations[i].minutes;
-    var percentageTaken =
+    const percentageTaken =
       (seriesDurationInMinutes / avarageLifeSpanInMinutes) * 100;
     totalPercentage += percentageTaken;
-    return (
-      seriesDurations[i].title + " took " + percentageTaken + "% of my life."
-    );
+    const obj = {};
+    obj[seriesDurations[i].title] = percentageTaken.toFixed(6) + "%";
+    arrayWithIndividualShow.push(obj);
   }
-  return "In total that is " + totalPercentage + "% of my life.";
+  const obj = {};
+  obj["Total"] = totalPercentage.toFixed(6) + "%";
+  arrayWithTotalAndIndividualShow.push(obj);
+  arrayWithTotalAndIndividualShow.push(arrayWithIndividualShow);
+  return arrayWithTotalAndIndividualShow;
 }
-var totalPercentage = 0;
-const avarageLifeSpan = 80;
-const avarageLifeSpanInMinutes = 80 * 365 * 24 * 60;
-
-console.log(logOutSeriesText());
+console.log(
+  "The % of my life taken by the total and individual show is given below: "
+);
+console.log(logOutSeriesText(100));
 
 /*Step 3: Smart-ease - Back to the basics!
 NOnoN0nOYes (Note taking app)*/
 
 const notes = [];
-var i = 0;
 function saveNote(content, id) {
-  notes.push({});
-  for (; i < notes.length; i++) {
-    notes[i].content = content;
-    notes[i].id = id;
-  }
+  notes.push({ content, id });
 }
 
 saveNote("Pick up groceries", 1);
@@ -110,31 +111,31 @@ function getNote(id) {
       if (notes[i].id === id) {
         return notes[i];
       } else {
-        const error1 = "Error: This id does not exist.";
-        return error1;
+        continue;
       }
     }
+    const error1 = "Error: This id does not exist.";
+    return error1;
   } else {
     const error2 = "Error: Please enter a number.";
     return error2;
   }
 }
 
-const firstNote = getNote(6);
-console.log(firstNote);
+//const firstNote = getNote(6);
+console.log(getNote(8));
 
 function logOutNotesFormatted() {
   for (var i = 0; i < notes.length; i++) {
-    return (
+    console.log(
       "The note with id: " +
-      notes[i].id +
-      ", has the following note text: " +
-      notes[i].content
+        notes[i].id +
+        ", has the following note text: " +
+        notes[i].content
     );
   }
 }
-
-console.log(logOutNotesFormatted());
+logOutNotesFormatted();
 
 /*Unique feature: to remove a note once done 
 using content and returns newNotes with 
@@ -150,7 +151,6 @@ function removeNote(content) {
   }
   return newNotes;
 }
-
 console.log(removeNote("Do laundry"));
 
 //CactusIO-interactive (Smart phone usage app) optional
@@ -158,14 +158,9 @@ const activities = [];
 var i = 0;
 function addActivity(data, activity, duration) {
   if (data === undefined || activity === undefined || duration === undefined) {
-    return "Add some activities before calling showStatus";
+    console.log("Add some activities before calling showStatus");
   }
-  activities.push({});
-  for (; i < activities.length; i++) {
-    activities[i].data = data;
-    activities[i].activity = activity;
-    activities[i].duration = duration;
-  }
+  activities.push({ data: data, activity: activity, duration: duration });
 }
 addActivity("23-7-2018", "Amazon Prime", 10);
 addActivity("23-7-2018", "Youtube", 30);
@@ -174,16 +169,15 @@ addActivity("24-7-2018", "Youtube", 30);
 addActivity("26-7-2018", "Amazon Prime", 30);
 console.log(activities);
 
-var totalUsage = 0;
-var j = 0; // extra feature to calculate with how many activities the usageLimite is reached.
-const usageLimit = 120;
-function showStatus() {
-  const totalActivities = activities.length;
-  for (var i = 0; i < activities.length; i++) {
+// Extra feature to calculate with how many activities the usageLimite is reached.
+function showStatus(usageLimit) {
+  let totalUsage = 0;
+  let j = 0;
+  for (let i = 0; i < activities.length; i++) {
     totalUsage += activities[i].duration;
     if (j === 0) {
-      if (totalUsage > usageLimit || totalUsage === usageLimit) {
-        var limitOfActivity = i + 1;
+      if (totalUsage >= usageLimit) {
+        let limitOfActivity = i + 1;
         console.log(
           "You have reached your usage limit with first " +
             limitOfActivity +
@@ -193,8 +187,9 @@ function showStatus() {
       }
     }
   }
-  return [totalActivities, totalUsage];
+  return [activities.length, totalUsage];
 }
+showStatus(120);
 const showMyStatus = showStatus(activities);
 console.log(
   "You have added " +
@@ -205,20 +200,18 @@ console.log(
 );
 //FOR OPTIONAL FEATURES:
 const newActivities = [];
-var i = 0;
-var today = new Date();
-var todaysDate =
-  today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
 function addActivityWithoutDate(activity, duration) {
+  var today = new Date();
+  var todaysDate =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
   if (activity === undefined || duration === undefined) {
     return "Add some activities before calling showStatus";
   }
-  newActivities.push({});
-  for (; i < newActivities.length; i++) {
-    newActivities[i].date = todaysDate;
-    newActivities[i].activity = activity;
-    newActivities[i].duration = duration;
-  }
+  newActivities.push({
+    date: todaysDate,
+    activity: activity,
+    duration: duration,
+  });
 }
 addActivityWithoutDate("Youtube", 30);
 addActivityWithoutDate("Facebook", 40);
@@ -226,42 +219,47 @@ addActivityWithoutDate("News", 30);
 
 console.log(newActivities);
 
-var k = 0;
+let k = 0;
+let todaysShowArray = [];
 function newShowStatus() {
+  var today = new Date();
+  var todaysDate =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
   for (i = 0; i < newActivities.length; i++) {
     if (newActivities[i].date === todaysDate) {
-      k = k + 1;
+      todaysShowArray.push(newActivities[i].activity);
+      k++;
     }
   }
   return k + " number of activities on " + todaysDate;
 }
-console.log(newShowStatus());
+console.log(`${newShowStatus()} and the activities are ${todaysShowArray}`);
 
 function userSpentMostTime() {
-  var array = activities;
-  var newArray = [];
+  let array = activities; // array containg all the activities with multiple occarances fx[x,y,z,x,x,y,z]
+  const newArray = []; //newArray will contain for each individual activity with added duration[x:total duration,y:total duration,z:total duration)
   j = 0;
   while (array.length != 0) {
-    var anotherArray = [];
-    k = 0;
-    newArray.push({});
-    newArray[j].duration = 0;
+    let anotherArray = []; // another array will cantain the rest
+    k = 0; //of activities while one acitivity is
+    newArray.push({}); //picked up in newArray to add the durations
+    newArray[j].duration = 0; //fx: newArray[X: summed up duration]
     for (i = 0; i < array.length; i++) {
       if (array[i].activity === array[0].activity) {
         newArray[j].activity = array[i].activity;
         newArray[j].duration += array[i].duration;
       } else {
-        anotherArray.push({});
+        anotherArray.push({}); // and in anotherArray[y,z,y,z]
         anotherArray[k].activity = array[i].activity;
         anotherArray[k].duration = array[i].duration;
         k++;
       }
     }
     j++;
-    array = anotherArray;
-  }
-  var temp_var = 0;
-  var most_used_activity = "";
+    array = anotherArray; // once oneactivity with summed up duration
+  } // is added to newArray and the rest of activities
+  let temp_var = 0; // added to anotherArray, anotherArray is assigned to array to continue for the next activity
+  let most_used_activity = "";
   for (x = 0; x < newArray.length; x++) {
     if (newArray[x].duration > temp_var) {
       temp_var = newArray[x].duration;
