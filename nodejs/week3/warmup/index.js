60ba1d3db3df89a47a5c8260e3e59a6053cd9953
rismita87 <rismita.ghosh@gmail.com>
2022-05-22 21:13:17 +0200

    implemented changes as per suggestion

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -3,14 +3,13 @@ const app = express();
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
-app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
 //Added 4 routes to index.js:
 //GET /calculator/add?first=<number here>&second=<number here>.
 //In response send sum (first + second).
 app.get("/calculator/add", async (request, response) => {
   const first = request.query.first;
   const second = request.query.second;
-  const sum = arrayContentAddition(first) + arrayContentAddition(second);
+  const sum = arrayWithNumbersToAdd(first) + arrayWithNumbersToAdd(second);
   response.send(sum.toString());
 });
 //GET /calculator/subtract?first=<number here>&second=<number here>.
@@ -19,7 +18,7 @@ app.get("/calculator/subtract", async (request, response) => {
   const first = request.query.first;
   const second = request.query.second;
   const subtract =
-    arrayContentSubtraction(first) - arrayContentSubtraction(second);
+    arrayWithNumbersToSubtract(first) - arrayWithNumbersToSubtract(second);
   response.send(subtract.toString());
 });
 
@@ -29,7 +28,7 @@ app.get("/calculator/multiply", async (request, response) => {
   const first = request.query.first;
   const second = request.query.second;
   const multiply =
-    arrayContentMultiplier(first) * arrayContentMultiplier(second);
+    arrayWithNumbersToMultiply(first) * arrayWithNumbersToMultiply(second);
   response.send(multiply.toString());
 });
 //GET /calculator/divide/<first number here>/<second number here>.
@@ -37,42 +36,33 @@ app.get("/calculator/multiply", async (request, response) => {
 app.get("/calculator/divide", async (request, response) => {
   const first = request.query.first;
   const second = request.query.second;
-  const divide = arrayContentDivision(first) / arrayContentDivision(second);
+  const divide =
+    arrayWithNumbersToDivide(first) / arrayWithNumbersToDivide(second);
   response.send(divide.toString());
 });
-function arrayContentAddition(arrayVar) {
-  let addoutput = 0;
-  if (arrayVar.length > 1) {
-    arrayVar.forEach((element) => {
-      addoutput = addoutput + parseInt(element);
-    });
-  } else addoutput = parseInt(arrayVar);
-  return addoutput;
+
+function arrayWithNumbersToAdd(param) {
+  const values = Array.isArray(param) ? param : [param];
+  const numbers = values.map((value) => parseInt(value));
+  return numbers.reduce((sum, number) => sum + number, 0);
 }
-function arrayContentSubtraction(arrayVar) {
-  let subtractionoutput = 0;
-  if (arrayVar.length > 1) {
-    arrayVar.forEach((element) => {
-      subtractionoutput = subtractionoutput + parseInt(element);
-    });
-  } else subtractionoutput = parseInt(arrayVar);
-  return subtractionoutput;
+
+function arrayWithNumbersToSubtract(param) {
+  const values = Array.isArray(param) ? param : [param];
+  const numbers = values.map((value) => parseInt(value));
+  return numbers.reduce((subtract, number) => subtract + number, 0);
 }
-function arrayContentMultiplier(arrayVar) {
-  let multioutput = 1;
-  if (arrayVar.length > 1) {
-    arrayVar.forEach((element) => {
-      multioutput = multioutput * parseInt(element);
-    });
-  } else multioutput = parseInt(arrayVar);
-  return multioutput;
+
+function arrayWithNumbersToMultiply(param) {
+  const values = Array.isArray(param) ? param : [param];
+  const numbers = values.map((value) => parseInt(value));
+  return numbers.reduce((multiply, number) => multiply * number, 1);
 }
-function arrayContentDivision(arrayVar) {
-  let divisionoutput = 0;
-  if (arrayVar.length > 1) {
-    arrayVar.forEach((element) => {
-      divisionoutput = divisionoutput + parseInt(element);
-    });
-  } else divisionoutput = parseInt(arrayVar);
-  return divisionoutput;
+
+function arrayWithNumbersToDivide(param) {
+  const values = Array.isArray(param) ? param : [param];
+  const numbers = values.map((value) => parseInt(value));
+  return numbers.reduce((multiply, number) => multiply + number, 0);
 }
+
+app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
